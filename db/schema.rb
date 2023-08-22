8fb75b3dbbe931e49ca33115627b5006d1d7158e
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-08-22 09:07:45 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2023_08_21_085619) do
 
   create_table "admins", force: :cascade do |t|
@@ -38,6 +39,14 @@ ActiveRecord::Schema.define(version: 2023_08_21_085619) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.boolean "is_deleted", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
