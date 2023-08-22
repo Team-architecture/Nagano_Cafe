53ee7e2bdc8415ca01fac74a13ac7737bece6b2d
Coba0405 <138278415+Coba0405@users.noreply.github.com>
2023-08-22 08:12:38 +0000

    ジャンルviwe,controller作成完了

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,35 @@
 class Admin::GenresController < ApplicationController
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def create
+    @ganre = Ganre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path, notice: "ジャンルを追加しました"
+    else
+      flash.now[:danger] = "エラーが発生しました"
+      @genres = Genre.all
+      render "index"
+  end
+  
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path, notice: "ジャンルを変更しました"
+    else
+      flash.now[:danger] = "エラーが発生しました"
+      render "edit"
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
