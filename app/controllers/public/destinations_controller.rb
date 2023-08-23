f80ed2c12543e7f2a409bf27908513ce83010a9b
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2023-08-23 12:01:57 +0000

    [NYD]CreateDestination

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -1,5 +1,20 @@
 class Public::DestinationsController < ApplicationController
   def index
+    @destination = Destination.new
+    @destinations = Destination.all
   end
-  
+
+  def create
+    @destination = Destination.new(destination_params)
+    @destination.save
+    redirect_to destinations_path
+  end
+
+
+  private
+
+  def destination_params
+    params.require(:destination).permit(:postal_code, :address, :name)
+  end
+
 end
