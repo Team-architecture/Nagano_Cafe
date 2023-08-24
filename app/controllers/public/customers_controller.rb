74433986455ce60a22cd4d1f02c61ff8d061d7b1
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-08-24 04:11:09 +0000

    taikai

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,17 +2,21 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
   def update
     customer = current_customer
     customer.update(customer_params)
     redirect_to customers_mypage_path
   end
-  
+
+  def confirm_withdraw
+    @customer = current_customer
+  end
+
   def withdraw
     @customer = Customer.find(current_customer.id)
     @customer.update(is_deleted: true)
@@ -20,9 +24,9 @@ class Public::CustomersController < ApplicationController
     flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kane, :postcode, :address, :phon_number, :email)
   end
