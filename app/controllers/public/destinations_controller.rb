7520de22397ba2843eec6b0d170adcb740f6d270
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-08-24 08:05:07 +0000

    配送

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -1,15 +1,42 @@
 class Public::DestinationsController < ApplicationController
   def index
     @destination = Destination.new
-    @destinations = Destination.all
+    @destinations = current_customer.destinations
+  end
+
+  def edit
+    @destination = Destination.find(params[:id])
   end
 
   def create
     @destination = Destination.new(destination_params)
-    @destination.save
+    @destination.customer_id = current_customer.id
+    if @destination.save
+      flash[:notice] = "配送先住所の作成が完了しました。"
     redirect_to destinations_path
+    else
+    @destinations = current_customer.destinations
+    render "index"
+    end
   end
 
+  def update
+    @destination = Destination.find(params[:id])
+     if @destination.update(destination_params)
+       flash[:notice] = "配送先の更新が完了しました。"
+       redirect_to destinations_path
+     else
+       render "edit"
+     end
+  end
+
+  def destroy
+    @destination = Destination.find(params[:id])
+    @destination.destroy
+    @destinations = current_customer.destinations
+    flash[:notice] = "配送先は正常に削除されました。"
+    redirect_to destinations_path
+  end
 
   private
 
