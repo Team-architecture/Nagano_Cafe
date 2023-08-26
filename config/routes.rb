a1275ec444d82d167d43d6c128787a737889ef0b
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2023-08-26 06:45:43 +0000

    FixAdminFunctions

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,8 @@ end
 
 # 管理者用
 # ↓2023/08/21記入
-scope module: :admin do
+namespace :admin do
+  get '/admin', to: 'homes#top', as: 'admin'
   resources :items, only: [:index, :new, :create, :show, :edit, :update]
   resources :genres, only: [:index, :create, :edit, :update]
   resources :customers, only: [:index, :show, :edit, :update]
@@ -42,7 +43,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
 
 
-
+  get 'homes/about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
