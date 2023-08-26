cc43306c33515a818751d9bb01c2ef044a3f8f45
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2023-08-26 05:37:20 +0000

    AddItem

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
 
 
-  get 'homes/about'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
