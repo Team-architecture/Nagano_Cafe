62b814681b430dccc6d73be421ee94ecb0e24d03
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-08-26 08:22:47 +0000

    admin

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,7 +7,7 @@ class Admin::GenresController < ApplicationController
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def create
     @ganre = Ganre.new(genre_params)
     if @genre.save
@@ -16,8 +16,9 @@ class Admin::GenresController < ApplicationController
       flash.now[:danger] = "エラーが発生しました"
       @genres = Genre.all
       render "index"
+    end
   end
-  
+
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
@@ -25,10 +26,11 @@ class Admin::GenresController < ApplicationController
     else
       flash.now[:danger] = "エラーが発生しました"
       render "edit"
+    end
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
