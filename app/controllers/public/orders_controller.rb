fc7c629665b3e0818e7919e975663d63838f1697
Coba0405 <138278415+Coba0405@users.noreply.github.com>
2023-08-27 07:18:40 +0000

    public/order修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,12 @@
 class Public::OrdersController < ApplicationController
+  # before_action :authenticate_custmaer!
   def new
     @order = Order.new
-    @orders = current_public.Orders.all
+    @orders = current_customer.orders.all
+    
   end
   
   def index
-    @orders = current_public.Orders.all  
+    @orders = current_customer.orders.all  
   end
 end
